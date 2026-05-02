d00c7d421522cf5e73191d4796dbbf7e689f0cc9
Keshav <ketiwari@ucsd.edu>
2026-05-01 18:36:43 -0700

    auto-scroll, send icon, ui fixes

--- archive/chat_interface/frontend/script.js
+++ archive/chat_interface/frontend/script.js
@@ -1,22 +1,24 @@
 async function send(){
     const val = document.getElementById("input").value
     console.log(val)
-    const text = document.createElement("div");
-    text.innerHTML = val
-    text.classList.add("user-input")
-    document.getElementById("chat-window").appendChild(text)
-    document.getElementById("input").value = "";
+    if (val){
+        const text = document.createElement("div");
+        text.innerHTML = val
+        text.classList.add("user-input")
+        document.getElementById("chat-window").appendChild(text)
+        document.getElementById("input").value = "";
 
-    const loading_text = document.createElement("div");
-    loading_text.innerHTML = "Thinking...";
-    loading_text.classList.add("llm-output");
-    document.getElementById("chat-window").appendChild(loading_text)
+        const loading_text = document.createElement("div");
+        loading_text.innerHTML = "Thinking...";
+        loading_text.classList.add("llm-output");
+        document.getElementById("chat-window").appendChild(loading_text)
 
-    const out_text = document.createElement("div");    
-    out_text.classList.add("llm-output");
+        const out_text = document.createElement("div");    
+        out_text.classList.add("llm-output");
 
-    await sample_call(val, out_text, loading_text);
-    //await llm_call(val, out_text, loading_text);
+        //await sample_call(val, out_text, loading_text);
+        await llm_call(val, out_text, loading_text);
+    } 
 
 }
 
@@ -35,7 +37,7 @@ async function llm_call(prompt, out_text, loading_text){
         
         const reader = response.body.getReader();
         const textDecoder = new TextDecoder();
-        result = ""
+        let result = ""
         
         const cursor = document.createElement("span");
         const text = document.createElement("span");
@@ -49,17 +51,20 @@ async function llm_call(prompt, out_text, loading_text){
 
         out_text.appendChild(text);
         out_text.appendChild(cursor);
-
+        
+        const chat_window = document.getElementById("chat-window")
         while (true){
             const {done, value} = await reader.read()
             if (done) {break} else{
                 text.innerHTML += textDecoder.decode(value);
+                chat_window.scrollTop = chat_window.scrollHeight;
                 result+= textDecoder.decode(value)
             }
         }
 
         cursor.remove()
         text.innerHTML = marked.parse(result)
+        chat_window.scrollTop = chat_window.scrollHeight;
 
     } catch(error){
         console.error(error.message)
