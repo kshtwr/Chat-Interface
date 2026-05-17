81b6640db6218d088cf24f1555e4e617bc73ec4b
Keshav <ketiwari@ucsd.edu>
2026-05-16 23:13:56 -0700

    WIP streaming messages

--- chat-interface-next/src/app/api/chat/route.ts
+++ chat-interface-next/src/app/api/chat/route.ts
@@ -6,15 +6,16 @@ export async function POST(request: Request) {
     const req = await request.json()
     
 
-    const msg = await client.messages.create({
+    const msg = client.messages.stream({
         model: "claude-haiku-4-5-20251001",
         max_tokens: 1024,
         messages: req.messages,
         system: "Keep answers brief."
       });
-
-    const textBlock = msg.content[0]
-    if (textBlock.type === "text") {
+    
+    const stream = await msg.finalMessage();
+    const textBlock = stream.content.find((block) => block.type == "text") // I need to read more about ReadableStream
+    if (textBlock && textBlock.type === "text") {
         return Response.json({ value: textBlock.text })
     }
       
